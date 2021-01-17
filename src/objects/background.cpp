0cee25aace1a8f5a7bdea4d858a0d83d92ff5ac1
Ray Redondo <rdrpenguin04@gmail.com>
2021-01-17 14:39:01 -0600

    Suppress MSVC Werror

--- src/objects/background.cpp
+++ src/objects/background.cpp
@@ -9,6 +9,7 @@ Background::Background(float x, float y, float z): x(x), y(y) {
 void Background::render(Renderer &renderer) {
     (void) x;
     (void) y;
+    (void) renderer;
     // I'll implement this properly once I have something for it.
 }
 
