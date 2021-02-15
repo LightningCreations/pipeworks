520414b6255b3f3ddee4b929e86ffa6fd64f8797
Ray Redondo <rdrpenguin04@gmail.com>
2021-02-14 21:34:07 -0600

    Add Doxygen for GameObject

--- include/gameobject.hpp
+++ include/gameobject.hpp
@@ -14,12 +14,32 @@ class GameObject;
 
 namespace pipeworks {
 
+/// \brief An object to be represented in the Engine.
 class GameObject {
   public:
     virtual ~GameObject() = 0;
-    virtual void render(Renderer &renderer) = 0; // Called from engine thread.
-    virtual void finished_loading() {} // Called from engine thread when all resources from this object are loaded. By default, does nothing.
-    virtual bool loaded() { return true; } // Called from engine thread to detect if resources need to be loaded before the object can be used.
+    /// \brief Render this object.
+    /// \param renderer The renderer to use.
+    /// \pre Calling this from any thread except the Engine thread results in undefined behavior.
+    ///
+    /// The Engine calls this from the Engine thread once per frame.
+    virtual void render(Renderer &renderer) = 0;
+    /// \brief Notify the object it is done loading.
+    /// \pre Calling this from any thread except the Engine thread results in undefined behavior.
+    /// \pre Calling this when some dependency resources aren't loaded yet results in undefined behavior.
+    ///
+    /// The Engine calls this from the Engine thread when all resources from this object are loaded.
+    /// The base implementation does nothing.
+    virtual void finished_loading() {}
+    /// \brief Determine if the GameObject's resources are all loaded.
+    /// \pre Calling this from any thread except the Engine thread results in undefined behavior.
+    ///
+    /// The base implementation has no resources to load, therefore it always returns `true`.
+    virtual bool loaded() { return true; }
+    /// \brief Get what resources may need to be loaded.
+    /// \pre Calling this from any thread except the Engine thread results in undefined behavior.
+    ///
+    /// The base implementation has no resources to load, therefore it always returns an empty vector.
     virtual std::vector<std::string> loadable_resources() { return std::vector<std::string>(); } // Called from engine thread to determine what resources need to be loaded.
 };
 
